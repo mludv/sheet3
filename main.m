27a8fedb29fecf4076d7855309a309e5a5d1bb3b
Maximilian Ludvigsson <maxiludvigsson@gmail.com>
2015-09-30 16:00:15 +0200

    2a fungerar

--- main.m
+++ main.m
@@ -39,30 +39,53 @@ eta = 0.01; %learning rate
 beta = 1/2;
 g = @(x) tanh(beta*x);     % activation function
 dg = @(x) beta*(1-g(x)^2); % derivative of g
+numPatterns = size(train_data,1);
+numValidPat = size(valid_data,1);
 
 % initialize weights and thresholds
-w = 0.2*rand(1,2);
-theta = 2*rand - 1;
+w = 0.4*rand(1,2) - 0.2;
+w(3)   = 2*rand - 1;
+
+% add a -1 to the data sets so we can subtract the threshold efficently
+train = [train_data(:,[1 2]) -1.*ones(numPatterns,1)];
+valid = [valid_data(:,[1 2]) -1.*ones(numValidPat,1)];
 
 % train network
 tmax = 10^6;
+Ht = zeros(1,tmax);
+Hv = zeros(1, tmax);
+Ct = zeros(1,tmax);
+Cv = zeros(1, tmax);
+%
 for i = 1:tmax
     % choose a pattern
-    zeta = datasample(train_data,1);
+    [pattern, index] = datasample(train,1);
+    real_output = train_data(index,3);
     
-    b = w * zeta([1 2])'-theta;
+    % calculate output
+    b = w * pattern';
     output = g(b);
     
-    error = dg(b)*(zeta(3)-output);
+    % calculate error
+    error = dg(b)*(real_output-output);
     
-    w = w + eta*error*zeta([1 2]);
-    theta = theta-eta*error;
+    % update weights
+    w = w + eta*error*pattern;
     
+    % save energies
+    Ht(i) = 0.5/numPatterns * sum((train_data(:, 3)' - g(w*train')).^2);
+    Ct(i) = 0.5/numPatterns * sum(abs(train_data(:, 3)' - g(w*train')));
+    Hv(i) = 0.5/numValidPat * sum((valid_data(:, 3)' - g(w*valid')).^2);
+    Cv(i) = 0.5/numValidPat * sum(abs(valid_data(:, 3)' - g(w*valid')));
+    
+    if rem(i, 10000) == 0
+        disp(i/10000)
+    end
 end
     
 %% Plot weights
 t = linspace(-3, 1.5);
-y = -w(1)/w(2)*t - theta/w(2);
+y = -w(1)/w(2)*t - w(3)/w(2);
 
 hold on
 plot(t, y)
